4f5f9a0e81fb5e004dc4ad36b664caf7c8c36fb4
Leonardo Motta de Oliveira <leonardomottadev@gmail.com>
2025-11-29 10:27:36 -0300

    feat(): sgcore-cloud

---
+++ sgcore-cloud/src/main/java/br/com/sgcore/sgcore_cloud/SgCoreCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgcore.sgcore_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgCoreCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgCoreCloudApplication.class, args);
+	}
+
+}
---
+++ sgcore-cloud/src/test/java/br/com/sgcore/sgcore_cloud/SgCoreCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgcore.sgcore_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgCoreCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
