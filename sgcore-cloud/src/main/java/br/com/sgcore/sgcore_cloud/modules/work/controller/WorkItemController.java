2590323bb13167516f6ac24d41d24bdd3b29cea1
Leonardo Motta de Oliveira <leonardomottadev@gmail.com>
2025-12-08 22:21:11 -0300

    feat(work): created WorkItem CRUD

--- sgcore-cloud/src/main/java/br/com/sgcore/sgcore_cloud/modules/work/controller/WorkItemController.java
+++ sgcore-cloud/src/main/java/br/com/sgcore/sgcore_cloud/modules/work/controller/WorkItemController.java
@@ -1,15 +1,59 @@
 package br.com.sgcore.sgcore_cloud.modules.work.controller;
 
+import br.com.sgcore.sgcore_cloud.modules.work.domain.WorkItem;
+import br.com.sgcore.sgcore_cloud.modules.work.dto.WorkItemDTO;
 import br.com.sgcore.sgcore_cloud.modules.work.service.WorkItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/work-item")
 @RequiredArgsConstructor
 public class WorkItemController {
 
-    private final WorkItemService workItemService;
+    private final WorkItemService service;
 
+    @PostMapping
+    public  ResponseEntity<?> insert(@Valid @RequestBody WorkItemDTO dto) {
+        WorkItem workItem = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(workItem.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WorkItemDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Collection<WorkItemDTO>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(
+            @PathVariable Long id,
+            @Valid @RequestBody WorkItemDTO dto) {
+
+        service.update(id, dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
